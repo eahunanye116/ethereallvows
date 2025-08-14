710bfef7d954d05a300070e8d19b7c3288ad85a8
Emmanuel Ahunanya <eahunanya16@gmail.com>
2025-08-14 23:38:54 +0000

    remove gift registry and all text should use Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
       </head>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/sections/Hero";
 import EventDetails from "@/components/sections/EventDetails";
 import RsvpFormSection from "@/components/sections/RsvpFormSection";
-import GiftRegistry from "@/components/sections/GiftRegistry";
 import PhotoGallery from "@/components/sections/PhotoGallery";
 import ThankYouNoteGenerator from "@/components/sections/ThankYouNoteGenerator";
 import Footer from "@/components/Footer";
@@ -13,7 +12,6 @@ export default function Home() {
         <Hero />
         <EventDetails />
         <RsvpFormSection />
-        <GiftRegistry />
         <PhotoGallery />
         <ThankYouNoteGenerator />
       </main>
